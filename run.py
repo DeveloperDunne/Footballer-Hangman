390dc7581046ab36f9e372957fa4a0673a340037
carl.dunne <developerdunne@gmail.com>
2023-12-14 11:02:02 +0000

    Added hangman pictures if guesses are wrong

--- run.py
+++ run.py
@@ -18,3 +18,56 @@ def reset(self):
 def chosen_player(self):
         footballers = [' messi', 'ronaldo', 'kane', 'haaland', 'mbappe', 'lewandowski', 'bellingham', 'neymar', 'son', 'foden', 'kroos', 'benzema', 'modric', 'salah', 'rashford', 'neuer', 'rooney', 'gerrard', 'lampard','maradona']
         return random.choice(footballers)
+
+"""Hangman pictures"""
+def hangman_picture(self):
+     if self.incorrect_guesses == 0:
+            print("________      ")
+            print("|      |      ")
+            print("|             ")
+            print("|             ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 1:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|             ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 2:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /       ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 3:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|      ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 4:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|\     ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 5:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|\     ")
+            print("|     /       ")
+            print("|             ")
+        else:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|\     ")
+            print("|     / \     ")
+            print("|             ")
+            print("YOU LOSE!")
